ebda49a29cb4ca3b787515852e574b685d054228
Marek Fořt <marekfort@me.com>
2020-04-20 22:30:24 +0200

    Add gh-pages.

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,6 +76,9 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    module.exports = {
+      pathPrefix: "/personal-web",
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
